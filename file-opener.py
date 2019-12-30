7781bd27a0b4a3199505835a3aa8322a7ea3bf38
kristinamiller <kfm287@gmail.com>
2019-12-29 20:33:00 -0800

    parses file data

--- file-opener.py
+++ file-opener.py
@@ -2,17 +2,27 @@ import os
 
 file_list = (os.listdir('/Users/kristinamiller/Documents/Freelancing/Genentech/test-folder1577676238'))
 files = {}
-print(file_list)
+# print(file_list)
 num_files = len(file_list)
+output = []
 
 os.chdir('/Users/kristinamiller/Documents/Freelancing/Genentech/test-folder1577676238')
 
+columns = ['name', 'email', 'boolean', 'squared']
+array = []
 for filename in file_list:
   with open(filename, 'r') as rf:
-    for line in rf:
-      if line.find('name') > -1:
-        print(line[line.find(':') + 2:].strip())
+    lines = rf.readlines()
+    for column in columns:
+      for line in lines:
+        if line.find(column) > -1:
+          output.append(line[line.find(':') + 2:].strip())
 
+  
+  
+# output.append(array)  
+
+print(output)
 
 
 
